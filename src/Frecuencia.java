e7418bf8487776a91cda48ed6a888fece1cf87b6
Irove04 <maresctor4@alu.edu.gva.es>
2024-12-05 11:41:53 +0100

    Frecuencia

--- src/Frecuencia.java
+++ src/Frecuencia.java
@@ -1,18 +1,15 @@
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Scanner;
 
 public class Frecuencia {
-   public static Map<String, Integer> frecuencia (String palabrasIngresadas){
-       String[] palabras = palabrasIngresadas.split(" ");
+   public static Map<String, Integer> frecuencia (String[] palabrasIngresadas){
        Map<String, Integer> map = new HashMap<>();
 
-       Integer contadorPalabras = 0;
+       Integer contadorPalabras;
 
        //mira si la palabra esta, si no esta la añade map.put(palabra, 1);
        //si esta, la suma map.put(palabra, contadorPalabras + 1);
-       for (String palabra : palabras) {
+       for (String palabra : palabrasIngresadas) {
            contadorPalabras  = map.get(palabra);
            if (contadorPalabras == null) {
                map.put(palabra, 1);
@@ -24,14 +21,8 @@ public class Frecuencia {
    }
 
 
-
-
-
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Ingresa una serie de palabras: (ejemplo: uno dos dos tres)");
-        String palabrasIngresadas = sc.nextLine();
-
+        System.out.println(frecuencia(new String[]{"uno", "dos", "dos", "tres", "tres"}));
 
     }
 }
